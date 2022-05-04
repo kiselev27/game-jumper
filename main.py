7c50e4ae8ca2fc19ffe9b47a44b3a945320e873b
kiselev27 <kovalova2205>
2022-05-05 01:12:03 +0300

    1. Добавлен фон 2. Алгорит прыжка 3. Движение лево/право 4. Спрайты героя и многое другое

--- main.py
+++ main.py
@@ -31,39 +31,5 @@ def main():
         hero.move()
         pygame.display.update()
 
-
-# def move(direction, hero_pos__X, hero_pos__Y, game, isJumping):
-    
-#     isJumping = False
-#     jumpCount = 10
-    
-#     if direction:
-#         if isJumping:
-#             if direction == K_LEFT and hero_pos__X > game.BORDER_WIDTH:
-#                 hero_pos__X -= game.STEP
-#             elif direction == K_RIGHT and hero_pos__X < game.SCREEN_WIDTH - game.hero_width - game.BORDER_WIDTH:
-#                 hero_pos__X += game.STEP
-#         elif not isJumping:
-#             if direction == K_SPACE:
-#                 isJumping = True
-                
-#                 if jumpCount >= -10:
-#                     if jumpCount < 0:
-#                         hero_pos__Y += (jumpCount ** 2) / 3
-#                     else:
-#                         hero_pos__Y -= (jumpCount ** 2) / 3
-#                     jumpCount -= 1
-#                 else:
-#                     isJumping = False
-#                     jumpCount = 10
-                    
-            # hero_pos__Y -= game.STEP
-        # elif direction == K_DOWN and hero_pos__y < game.BORDER_HEIGHT - game.BORDER_WIDTH:
-        #     hero_pos__Y += game.STEP
-            
-        
-            
-    # return hero_pos__X, hero_pos__Y, isJumping
-
 if __name__ == '__main__':
     main()
